4d9c0ab63893d57e7fa0532a72ec8ace9fa47262
nursah <nursahari@hotmail.com>
2022-01-12 22:03:08 +0300

    Initial Commit

---
+++ GuesstheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuesstheFlag
+//
+//  Created by Nurşah on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
